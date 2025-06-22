19140bf3ed1de4aa0a55f6ac18099673b62877ac
Sam Phillips <srp60@cam.ac.uk>
2025-06-22 20:43:09 +0100

    refactor: remove Flask-Migrate integration and related migration files; update app initialization to create database tables on startup

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,8 @@
+import os
 from flask import Flask
 from config import Config
 from flask_cors import CORS
 from .database_models import db
-from flask_migrate import Migrate
 
 def create_app(config_class=Config):
     app = Flask(__name__)
@@ -13,11 +13,12 @@ def create_app(config_class=Config):
 
     # Initialize database
     db.init_app(app)
-    from . import database_models
-    migrate = Migrate(app, db)
 
     # Import and register blueprints here
     from app.routes import bp as main_bp
     app.register_blueprint(main_bp)
+
+    with app.app_context():
+        db.create_all()
 
     return app 
